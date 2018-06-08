b51a07fd0c0f92bb83da73b261875d75d66c9fa0
prune <prune@lecentre.net>
2018-06-08 13:40:58 -0400

    first commit with code

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # docker-registry-cleaner
 a Go application to remove images from a remote Docker Registry
+
+## Usage
+```
+./docker-registry-cleaner  -project=kube00-XXX -registryURL="https://us.gcr.io" -username='_token' -password=$(gcloud auth print-access-token) -logLevel=info -filter="^kube00-XXX/us.gcr.io/imagename/imagename2:1234"
+```
+
+- registry URL: the URL to reach the registry. Do not add the `/v2` endpoint
+  - Google: `https://us.gcr.io` for the US registry. Can also be `https://gcr.io`
+- project: the name of the project (keep empty is no project is used)
+  - Google: find your project with `gcloud projects list`
+- username: your Registry username.
+    - Google: `_token`
+- password: well, your password
+    - Google: get your token password with `gcloud auth print-access-token`
+- logLevel: one of debug, info, warning, error. When using `debug` you will also get the HTTP calls to the Registry.
+- filter: a RegExp to filter which images to list/delete. Leave empty to list/delete all images.
+  
+- delete: when selected, will delete the manifest (the image) from the registry.
+
+## status
+As of now this tool is working to list images from a Google registry.
+It does not work to remove images. The error is :
+```
+Delete https://us.gcr.io/v2/kube00-XXXX/imagename/imagename2/manifests/sha256:5104db36afd2ea4a3977174e8ee1ce0fcec5678401a50d1a1cbcf240f2fd7da2: 
+http: non-successful response 
+status=404 
+body={
+    "errors":[
+        {"code":"NAME_UNKNOWN",
+         "message":"Failed to compute blob liveness for manifest: 'sha256:5104db36afd2ea4a3977174e8ee1ce0fcec5678401a50d1a1cbcf240f2fd7da2'"
+         }]
+    }
+```
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"regexp"
+
+	"github.com/prune998/docker-registry-client/registry"
+	"github.com/sirupsen/logrus"
+
+	"os"
+
+	"github.com/namsral/flag"
+)
+
+var (
+	// version is filled by -ldflags  at compile time
+	version     = "no version set"
+	registryURL = flag.String("registryURL", "https://us.gcr.io", "The Docker Registry URL")
+	project     = flag.String("project", "", "The Docker project, if using gcloud registry")
+	username    = flag.String("username", "", "The Docker Registry user name, use '_token' if using a a gcloud generated token")
+	password    = flag.String("password", "", "The Docker Registry password. use 'gcloud auth print-access-token' if connecting to gcloud")
+	logLevel    = flag.String("logLevel", "warn", "log level from debug, info, warning, error")
+	delete      = flag.Bool("delete", false, "delete the selected images")
+	filter      = flag.String("filter", "", "regular expression matching the image/tag to remove")
+	log         = logrus.New()
+)
+
+func main() {
+	flag.Parse()
+
+	log.Out = os.Stdout
+	log.Formatter = new(logrus.JSONFormatter)
+	log.Level, _ = logrus.ParseLevel(*logLevel)
+
+	if *delete && *filter == "" {
+		log.Fatalf("you have to set a -regexp when using -delete")
+	}
+
+	r := regexp.MustCompile(*filter)
+
+	// connect to the Docker Registry
+	hub, err := registry.New(*registryURL, *username, *password, log.Debugf)
+	if err != nil {
+		log.Fatalf("error connecting to hub, %v", err)
+	}
+
+	// get repository (images) list
+	repositories, err := hub.Repositories()
+	if err != nil {
+		log.Fatalf("repositories error, %v", err)
+
+	}
+
+	for _, repo := range repositories {
+		// search for each image tag (version)
+		images, err := hub.Tags(repo)
+		if err != nil {
+			log.Fatalf("Listing image error, %v", err)
+		}
+		for _, image := range images {
+			// we join the image tag with the repository name to check
+			// against the regular expression
+			fullRepo := repo + ":" + image
+
+			if r.MatchString(fullRepo) {
+				digest, err := hub.ManifestDigest(repo, image)
+
+				if *delete {
+					err = hub.DeleteManifest(repo, digest)
+					if err != nil {
+						log.Fatalf("error deleting %v: %v", digest, err)
+					}
+				}
+				log.WithFields(logrus.Fields{
+					"repository": repo,
+					"image":      image,
+					"delete":     *delete,
+					"fullname":   fullRepo,
+					"digest":     digest,
+				}).Printf("image found")
+			}
+		}
+
+	}
+
+}
